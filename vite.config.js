9fb27eb13915fc59a9f29bc9e5db7df2eb81eea9
Keerthana M <keerthanam.cse2022@gitchennai.net>
2025-02-11 09:04:44 +0530

    Fix Vite issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isGitHubPages = import.meta.env.MODE === "production";
-
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubPages ? "/Cryptoplace/" : "/", 
+  base: import.meta.env.MODE === "production" ? "/Cryptoplace/" : "/", 
 });
